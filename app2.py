e0d2f5daba6489ac325d818a240bfa91ef949e06
Sanches <smtaicho5@gmail.com>
2022-11-17 19:43:43 -0300

    Versão 1

---
+++ app2.py
@@ -0,0 +1,13 @@
+# ​Código utilizado na aula:
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
